485721ffedf350a3274fe964e154736855a9633e
Senudes ✘◡✘ <36734463+syndicate1984@users.noreply.github.com>
2020-03-15 08:59:32 +0200

    Update tempban.sql

--- tempban.sql
+++ tempban.sql
@@ -23,6 +23,10 @@ CREATE TABLE IF NOT EXISTS `vrp_users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
+ALTER TABLE vrp_users ADD IF NOT EXISTS tempban varchar(150) NOT NULL DEFAULT 'No Temp Ban' ;
+ALTER TABLE vrp_users ADD IF NOT EXISTS bannedReason varchar(150) NOT NULL DEFAULT 'No Ban Reason' ;
+ALTER TABLE vrp_users ADD IF NOT EXISTS bannedBy varchar(150) NOT NULL DEFAULT 'No Ban By' ;
+
 -- Data exporting was unselected.
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
